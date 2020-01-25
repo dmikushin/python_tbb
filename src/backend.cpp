d860a10eed6ed96564cb7c1770acab1fc26b424d
Dmitry Mikushin <dmitry@kernelgen.org>
2020-01-25 15:06:22 +0100

    Implementing Python vs TBB test case

---
+++ src/backend.cpp
@@ -0,0 +1,52 @@
+#include <functional>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_for.h>
+#include <tbb/task_arena.h>
+#include <tbb/task_group.h>
+#include <tbb/task_scheduler_init.h>
+#include <vector>
+
+PYBIND11_MAKE_OPAQUE(std::vector<int>);
+
+using namespace std;
+
+void backend_tbb(vector<int>& result, std::function<int (int)>& callback)
+{
+	int nthreads = tbb::task_scheduler_init::default_num_threads();
+	const char* cnthreads = getenv("TBB_NUM_THREADS");
+	if (cnthreads) nthreads = std::max(1, atoi(cnthreads));
+
+	tbb::task_group group;
+	tbb::task_arena arena(nthreads, 1);
+	tbb::task_scheduler_init init(nthreads);
+
+	group.run( [&] {
+		tbb::parallel_for(tbb::blocked_range<int>(0, result.size()),
+			[&](const tbb::blocked_range<int>& range)
+		{
+			for (int i = range.begin(); i != range.end(); i++)
+				result[i] = callback(i);
+		});
+	});
+
+	arena.execute( [&] { group.wait(); });		
+}
+
+void backend_serial(vector<int>& result, std::function<int (int)>& callback)
+{
+	for (int i = 0; i < result.size(); i++)
+		result[i] = callback(i);
+}
+
+PYBIND11_MODULE(python_tbb, m)
+{
+	pybind11::bind_vector<std::vector<int> >(m, "stdvectorint");
+
+	m.def("backend_tbb", &backend_tbb, "TBB backend");
+	m.def("backend_serial", &backend_serial, "Serial backend");
+}
+
---
+++ src/frontend.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from python_tbb import *
+import numpy as np
+
+def callback(a) :
+	return int(a) * 10
+
+def main() :
+	length = 10
+	result1 = stdvectorint(np.zeros(length, np.int32))
+	result2 = stdvectorint(np.zeros(length, np.int32))
+	
+	backend_serial(result1, callback)
+        # Uncomment this to get the program hang
+	#backend_tbb(result2, callback)
+
+	for i in range(length) :
+		print("%d vs %d" % (result1[i], result2[i]))
+	
+if __name__ == "__main__" :
+	main()
+
